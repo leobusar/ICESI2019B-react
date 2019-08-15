2fca703ad23c85308bfa71d9919c8b74b1bb4ce0
Leonardo <leobusta@gmail.com>
2019-08-15 18:10:31 -0500

    clase  introduccion react

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  constructor(){
+      super()
+      this.state = {
+          numEmpl: 0,
+          direccion: {
+                calle: "12A",
+                numero: "14-10"
+          }
+      }
+   }
+
+   componentWillMount(){
+       this.setState({
+           numEmpl: 10
+       })
+   }
+
+  render() {
+    return (
+       <div className="container">
+        <h1>{this.props.corporation}</h1>
+        <h2>Empleados: {this.state.numEmpl} </h2>
+        </div> 
+    );
+  }
+}
+
+export default Header;
